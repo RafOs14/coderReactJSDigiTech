5d2feba4e1697025e2fdf2833a947ea53ba1f4db
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-03-28 10:16:06 -0300

    Update CartWidget.js

--- digitech/src/components/CartWidget.js
+++ digitech/src/components/CartWidget.js
@@ -1,8 +1,10 @@
+//Componente que dibuja el icono del carrito, el cual se usara en el navbar
 import React, {Component} from "react";
 import boostrap from "bootstrap/dist/css/bootstrap.css";
 import {Badge} from "@material-ui/core";
 import { ShoppingCartOutlined } from "@material-ui/icons";
 
+//Declaramos la clase y lo que se va a renderizar
 class CartWidget extends React.Component{
     render(){
         return (
@@ -14,5 +16,6 @@ class CartWidget extends React.Component{
     }
 }
 
+//Exportamos el componente
 export default CartWidget;
 
