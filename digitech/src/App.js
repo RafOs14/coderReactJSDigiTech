6213ec56339f78d7bd1e1266914ad450634dfd18
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-03-28 01:13:11 -0300

    Update App.js

--- digitech/src/App.js
+++ digitech/src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
     <div className="App">      
         <NavBar />
-      
+        <ItemListContainer greeting = "Proximamente"/> 
     </div>
   );
 }
