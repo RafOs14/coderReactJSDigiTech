7c50119a49837a82465c13056e2dd353e3962096
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-03-29 00:14:15 -0300

    Update ItemListContainer.js

--- digitech/src/components/ItemListContainer.js
+++ digitech/src/components/ItemListContainer.js
@@ -1,6 +1,7 @@
 //Componente que dibuja el cuerpo de la pagina
 import React, {Component} from "react";
 import boostrap from "bootstrap/dist/css/bootstrap.css";
+import ItemCount from "./ItemCount";
 
 
 //Declaramos la clase que va a renderizar el componente y recibir las props
@@ -18,8 +19,10 @@ class ItemListContainer extends React.Component{
                                 <div className="row">
                                     <div className="col-12">
                                         <div className="table-responsive">
-                                            //Utilizamos las props declaradas en el componente App.js
+                                            {/*Utilizamos las props declaradas en el componente App.js*/}
                                             {this.props.greeting}
+                                            {/*Llamamos al componente ItemCount y le pasamos dos parametros*/}
+                                            <ItemCount stock="10" initial="1"/>
                                         </div>
                                     </div>
                                 </div>
