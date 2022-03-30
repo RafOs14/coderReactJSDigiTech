791bd0354f5e6c4944f28e5a078355f7f8396ded
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-03-30 20:19:25 -0300

    Update ItemCount.js

--- digitech/src/components/ItemCount.js
+++ digitech/src/components/ItemCount.js
@@ -1,16 +1,29 @@
-//Componente que dibuja el icono del carrito, el cual se usara en el navbar
 import boostrap from "bootstrap/dist/css/bootstrap.css";
-import {Badge} from "@material-ui/core";
-import { ShoppingCartOutlined } from "@material-ui/icons";
+import { Button } from "bootstrap";
+import { Add, Remove } from "react-bootstrap-icons";
+import { Card } from "react-bootstrap";
+import cougar from "../assets/cougar.jpg";
+
 
 //Declaramos la clase y lo que se va a renderizar
-const CartWidget = () => {  
+const ItemCount = ({stock,initial,onAdd}) => {  
         return (
-            <Badge badgeContent={4} color="primary">
-                <ShoppingCartOutlined style={{color : "white"}}/>
-            </Badge>
+            <div className="d-flex">
+                <Card style={{ width: "18rem" }}>
+                    <Card.Img variant="top" src={cougar} />
+                    <Card.Body>
+                        <Card.Title>Auriculares Cougar</Card.Title>
+                        <Card.Text>
+                            <Button variant="outlined-primary"><Add /></Button>
+                            <span className="mx-5">{initial}</span>
+                            <Button variant="outlined-primary"><Remove /></Button>
+                        </Card.Text>
+                        <Button variant="outlined-primary">Agregar al carrito</Button>
+                    </Card.Body>
+                </Card>
+            </div>
         );    
 }
 
 //Exportamos el componente
-export default CartWidget;
+export default ItemCount;
