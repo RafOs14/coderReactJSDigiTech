b684ec2a653b8d14a0903dd44684a21768ebace9
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-03-30 17:41:10 -0300

    Update NavBar.js

--- digitech/src/components/NavBar.js
+++ digitech/src/components/NavBar.js
@@ -1,16 +1,16 @@
 //Componente que crea un navbar
-import React, {Component} from "react";
 import boostrap from "bootstrap/dist/css/bootstrap.css";
+import { AppBar } from "@material-ui/core";
 import logo from "../assets/logo.png";
 import CartWidget from "./CartWidget";
 
-class NavBar extends React.Component{
-    render(){
-        return (  
-            <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark p-4 mb-2">
+const NavBar = () => {
+        {/*Retornamos lo que va a mostrar el componente principal*/}
+        return (            
+            <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark px-5 mb-2">
                 <img src={logo} alt="logo" className="logo" />                
                 <a><span className="fw-bold h4 text-white mx-3">DigiTech</span></a>                
-                <div className="collapse navbar-collapse " id="navbarSupportedContent">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
                             <a className="nav-link active" aria-current="page" href="#">Informática</a>
@@ -28,9 +28,9 @@ class NavBar extends React.Component{
                             <CartWidget />
                     </div>
                 </div>
-            </nav>
-        );
-    }
+            </nav>            
+        );    
 }
 
+//Exportamos el componente
 export default NavBar;
