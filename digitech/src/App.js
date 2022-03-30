159ada273cea3f969dc4516bbf1855c4ae177d78
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-03-30 17:42:11 -0300

    Update App.js

--- digitech/src/App.js
+++ digitech/src/App.js
@@ -1,19 +1,11 @@
 //Realizamos los imports correspondientes, entre ellos los distintos componentes que arman la app
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer';
+import Home  from './pages/Home';
 
-function App() {
-  return (
-    <div className="App"> 
-      //Importamos el componente NavBar, el cual renderiza el navbar
-        <NavBar />
-    //Importamos el componente ItemListContainer, el cual renderiza el cuerpo
-        <ItemListContainer greeting = "Proximamente"/> 
-    </div>
-  );
+const  App = () => {
+  {/*Importamos el componente Home, el cual renderiza la pagina*/}
+  return  <Home />;
 }
 
 export default App;
