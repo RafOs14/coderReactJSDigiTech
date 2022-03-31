9dca727578bd43183438132ef1f17b4c37eb58b0
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-03-30 22:45:26 -0300

    Update ItemListContainer.js

--- digitech/src/components/ItemListContainer.js
+++ digitech/src/components/ItemListContainer.js
@@ -1,20 +1,21 @@
 //Componente que dibuja el cuerpo de la pagina
 import boostrap from "bootstrap/dist/css/bootstrap.css";
-import {Button, Wrapper} from "react-bootstrap";
 import ItemCount from "./ItemCount";
-import cougar from "../assets/cougar.jpg";
 
-
-//Declaramos la clase que va a renderizar el componente y recibir las props
+//Componente que contiene a los artículos
 const ItemListContainer = ({greeting}) => {
-    //Declaramos el estado de la cantidad de items    
+    //Mostramos una alerta al agregar los items
+    const onAdd = (items) => {
+        alert(`${items} items added to cart`);
+    }
+
         return (  
-            <div className="container">
+            <div className="container mt-2">
                 <div className="row">
                     <div className="col-12">
                         <div className="card">
                             <div className="card-header">
-                                <h3 className="card-title">Listado de productos</h3>
+                                <h3 className="card-title ">Products</h3>
                             </div>
                             <div className="card-body">
                                 <div className="row">
@@ -22,8 +23,8 @@ const ItemListContainer = ({greeting}) => {
                                         <div className="table-responsive">
                                             {/*Utilizamos las props declaradas en el componente App.js*/}
                                             {greeting}
-                                            {/*Llamamos al componente ItemCount y le pasamos dos parametros*/}
-                                            <ItemCount />
+                                            {/*Llamamos al componente ItemCount y le pasamos dos parametros*/}                                           
+                                            <ItemCount stock={5} initial={1} onAdd={onAdd}/>
                                         </div>
                                     </div>
                                 </div>
