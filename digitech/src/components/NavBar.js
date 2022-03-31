679b5e4efdc3e4fa57821cfee4d8bb43d42f7d49
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-03-30 22:46:29 -0300

    Update NavBar.js

--- digitech/src/components/NavBar.js
+++ digitech/src/components/NavBar.js
@@ -1,34 +1,48 @@
 //Componente que crea un navbar
 import boostrap from "bootstrap/dist/css/bootstrap.css";
-import { AppBar } from "@material-ui/core";
 import logo from "../assets/logo.png";
 import CartWidget from "./CartWidget";
+import { Navbar, Nav } from 'react-bootstrap';
 
 const NavBar = () => {
-        {/*Retornamos lo que va a mostrar el componente principal*/}
-        return (            
-            <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark px-5 mb-2">
-                <img src={logo} alt="logo" className="logo" />                
-                <a><span className="fw-bold h4 text-white mx-3">DigiTech</span></a>                
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Informática</a>
+        /*Retornamos lo que va a mostrar el componente principal*/
+        return ( 
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid mx-2">
+                    <img src={logo} alt="logo" className="navbar-brand" />
+                    <a className="navbar-brand" href="#">DigiTech</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+                        <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Computer</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" aria-current="page" href="#">Telefonía</a>
+                        <a className="nav-link" href="#">Video Games</a>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link active" href="#">VideoJuegos</a>
-                        </li>
+                        <a className="nav-link" href="#">Cell Phones</a>
+                        </li>                                                 
                     </ul>
-                    <div class="d-flex">
-                            <a className="nav-link link-light" href="#">Registrarse</a> 
-                            <a className="nav-link link-light" href="#">Iniciar Sesión</a>
-                            <CartWidget />
+                    <form className="d-flex">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+                            <li className="nav-item">
+                            <a className="nav-link" aria-current="page" href="#">Register</a>
+                            </li>
+                            <li className="nav-item">
+                            <a className="nav-link" href="#">Sing In</a>
+                            </li>
+                            <li className="nav-item">
+                            <a className="nav-link" href="#">Sing Up</a>
+                            </li>                                                 
+                        </ul>                        
+                    </form>
+                    <CartWidget />
                     </div>
                 </div>
-            </nav>            
+        </nav>
         );    
 }
 
