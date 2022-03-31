e69e0df49489ebd40bed01f3c46c2fc4ac1bb359
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-03-30 22:47:12 -0300

    Update App.js

--- digitech/src/App.js
+++ digitech/src/App.js
@@ -1,11 +1,9 @@
-//Realizamos los imports correspondientes, entre ellos los distintos componentes que arman la app
 import './App.css';
 import React from 'react';
-import Home  from './pages/Home';
+import Home from './pages/Home';
 
-const  App = () => {
-  {/*Importamos el componente Home, el cual renderiza la pagina*/}
-  return  <Home />;
+const App = () => {
+  return <Home /> ;   
 }
 
 export default App;
