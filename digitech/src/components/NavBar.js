b793708df9fc61b0f44d1036a2b4b7fa5cec97fc
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-04-03 23:35:49 -0300

    Update NavBar.js

--- digitech/src/components/NavBar.js
+++ digitech/src/components/NavBar.js
@@ -1,19 +1,19 @@
 //Componente que crea un navbar
 import logo from "../assets/logo.png";
 import CartWidget from "./CartWidget";
-import { Container, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Container, Navbar, Nav } from 'react-bootstrap';
 
 const NavBar = () => {
         /*Retornamos lo que va a mostrar el componente principal*/
         return ( 
             <Navbar bg="dark" variant="dark" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">
+                    <Navbar.Brand href="#home" gap={2}>
                         <img
                         alt=""
                         src={logo}
                         width="40"
-                        height="40"
+                        height="30"
                         className="d-inline-block align-top"
                         />{' '}
                         DigiTech
@@ -25,9 +25,12 @@ const NavBar = () => {
                         <Nav.Link href="#link">Video Games</Nav.Link> 
                         <Nav.Link href="#link">Cell Phones</Nav.Link>        
                     </Nav>                   
-                    <Nav className="mx-3">
-                        <CartWidget />
-                    </Nav>
+                    <Nav className="mx-4">
+                        <Nav.Link href="#link">Register</Nav.Link>
+                        <Nav.Link href="#link">Sign In</Nav.Link> 
+                        <Nav.Link href="#link">Sign Up</Nav.Link>                         
+                    </Nav>                    
+                    <CartWidget />
                     </Navbar.Collapse>
                 </Container>
             </Navbar>            
