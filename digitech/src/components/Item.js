8fc4994764ccaa3b1fb146a0f453e2cbfd604a6c
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-04-03 23:52:43 -0300

    Update Item.js

--- digitech/src/components/Item.js
+++ digitech/src/components/Item.js
@@ -4,24 +4,22 @@ import ItemCount from "./ItemCount";
 export default function Item ({ id, title, stock, cost, brand, image }) {
 
     return (     
-        <div>
-            <Container className="mt-3"> 
-                <Row> 
-                    <Col md={5}>
-                        <Card key={id}>
-                            <CardImg top width="100%" src={image} alt="Card image cap" />
-                            <Card.Body>
-                                <Card.Title className="text-center font-weight-bolder" style={{ color : 'blue'}}>{title}</Card.Title>
-                                <Card.Subtitle className="text-center font-weight-bolder" style={{ color : 'red' }}>USD: {cost}</Card.Subtitle>
-                                <Card.Text className="text-center"></Card.Text>
-                                <Card.Text className="text-center">Brand: {brand}</Card.Text>                                
-                                <ItemCount />
-                                <Card.Text className="text-center mt-2">Stock: {stock}</Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>  
-        </div>
+        <Container flex> 
+            <Row> 
+                <Col>
+                    <Card key={id} >
+                        <CardImg top width="100%" src={image} alt="Card image cap" />
+                        <Card.Body>
+                            <Card.Title className="text-center font-weight-bolder" style={{ color : 'blue'}}>{title}</Card.Title>
+                            <Card.Subtitle className="text-center font-weight-bolder" style={{ color : 'red' }}>USD: {cost}</Card.Subtitle>
+                            <Card.Text className="text-center"></Card.Text>
+                            <Card.Text className="text-center">Brand: {brand}</Card.Text>                                
+                            <ItemCount />
+                            <Card.Text className="text-center mt-2">Stock: {stock}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>          
     );
 }
