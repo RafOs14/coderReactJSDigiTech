e6ea47806899676bb496532061dbfa28e64a8521
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-04-03 22:48:55 -0300

    Create ItemList.js

---
+++ digitech/src/components/ItemList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Item from "./Item";
+
+const ItemList = ({ items }) => {
+    return (
+        <>        
+            { 
+                //Recorremos el arreglo de productos para mostrarlo en pantalla
+                items.length > 0
+                ?items.map(item => <Item key={item.id} id={item.id} title={item.description} cost={item.price} brand={item.brand} image={item.image[0]} stock={item.stock} />)
+                : <div className="text-center mt-3">Products will be displayed in an instant...</div>                    
+            }          
+        </>
+    );
+}
+
+export default ItemList;
