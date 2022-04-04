a5697e29e7ce606e06253486668a03d4a0cd7e68
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-04-03 22:48:25 -0300

    Update ItemCount.js

--- digitech/src/components/ItemCount.js
+++ digitech/src/components/ItemCount.js
@@ -1,12 +1,9 @@
 import { useEffect, useState } from "react";
-import boostrap from "bootstrap/dist/css/bootstrap.css";
 import { Button } from "react-bootstrap";
 import { Add, Remove } from '@material-ui/icons'
-import { Card } from "react-bootstrap";
-import cougar from "../assets/cougar.jpg";
 
 //Declaramos la clase y lo que se va a renderizar
-const ItemCount = ( {stock, initial, onAdd}) => { 
+export default function ItemCount ({stock, initial, onAdd}) { 
         //Declaramos el estado de la cantidad de items  
         const [count, setCount] = useState(0);
         
@@ -27,32 +24,20 @@ const ItemCount = ( {stock, initial, onAdd}) => {
         }
         //Retornamos el componente
         return (
-            <div className="d-flex">
-                <Card className="shadow" style={ { width: '18rem' } }>
-                    <Card.Img variant="top" src={cougar} />
-                    <Card.Body>
-                        <Card.Title className="text-center">Cougar HeadSet</Card.Title>
-                        <Card.Text className="text-center">
-                            <span className="fw-bold">Usd 150</span>
-                        </Card.Text>
-                        <Card.Text className="mx-4">
-                            <Button variant="outline-danger" onClick={remove}><Remove /></Button>
-                            <span className="mx-5">{count}</span>
-                            <Button variant="outline-success" onClick={add}><Add /></Button>
-                        </Card.Text>                        
-                        <div className="d-grid gap-2">
-                            {
-                                stock
-                                ?<Button variant="primary" size="lg" onClick={() => onAdd(count)}>Add to Cart</Button>
-                                :<Button variant="danger" size="lg" disabled>Out of Stock</Button>
-                            }
-                            
-                        </div>
-                    </Card.Body>                   
-                </Card>
+            <div className="d-grid gap-2">
+                <div className="mx-4 text-center">
+                <Button variant="outline-danger" onClick={remove}><Remove /></Button>
+                    <span className="mx-5">{initial}</span>
+                <Button variant="outline-success" onClick={add}><Add /></Button>
+                </div>                        
+                <div className="d-grid gap-2">
+                    {
+                        stock
+                        ?<Button variant="primary" size="lg" onClick={() => onAdd(count)}>Add to Cart</Button>
+                        :<Button variant="danger" size="lg" disabled>Out of Stock</Button>
+                    }                         
+                </div>
             </div>
         );    
 }
 
-//Exportamos el componente
-export default ItemCount;
