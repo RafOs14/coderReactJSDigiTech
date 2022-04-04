8921d4cf130b4cb95c60637828c765bb9de88a54
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-04-03 22:49:40 -0300

    Update ItemListContainer.js

--- digitech/src/components/ItemListContainer.js
+++ digitech/src/components/ItemListContainer.js
@@ -1,38 +1,28 @@
 //Componente que dibuja el cuerpo de la pagina
-import boostrap from "bootstrap/dist/css/bootstrap.css";
-import ItemCount from "./ItemCount";
+import ItemList from "./ItemList";
+import { useEffect, useState } from "react";
+import { getProducts } from "../utils/products";
 
 //Componente que contiene a los artículos
-const ItemListContainer = ({greeting}) => {
+const ItemListContainer = () => {
+    const [datos, setProductos] = useState([]);
+
     //Mostramos una alerta al agregar los items
     const onAdd = (items) => {
         alert(`${items} items added to cart`);
     }
+    
+    useEffect(() => {
+        //Obtenemos los productos con una promesa
+        async function fetchData() {
+            let data = await getProducts();
+            setProductos(data);
+            }
+        fetchData();
+        }, []);
 
-        return (  
-            <div className="container mt-2">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="card">
-                            <div className="card-header">
-                                <h3 className="card-title ">Products</h3>
-                            </div>
-                            <div className="card-body">
-                                <div className="row">
-                                    <div className="col-12">
-                                        <div className="table-responsive">
-                                            {/*Utilizamos las props declaradas en el componente App.js*/}
-                                            {greeting}
-                                            {/*Llamamos al componente ItemCount y le pasamos dos parametros*/}                                           
-                                            <ItemCount stock={5} initial={1} onAdd={onAdd}/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>        
+        return (               
+            <ItemList items={datos} initial={1} onAdd={onAdd}/>           
         );          
 }
 
