3195270a22bdfddc9135d7a37e6cfe028bcb0aea
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-04-04 22:51:05 -0300

    Update ItemCount.js

--- digitech/src/components/ItemCount.js
+++ digitech/src/components/ItemCount.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { Add, Remove } from '@material-ui/icons'
 
 //Declaramos la clase y lo que se va a renderizar
@@ -24,19 +24,17 @@ export default function ItemCount ({stock, initial, onAdd}) {
         }
         //Retornamos el componente
         return (
-            <div className="d-grid gap-2">
-                <div className="mx-4 text-center">
-                <Button variant="outline-danger" size="sm" onClick={remove}><Remove /></Button>
-                    <span className="mx-5">{initial}</span>
+           <Card.Text className="text-center">
+               <Button variant="outline-danger" size="sm" onClick={remove}><Remove /></Button>
+                <span className="mx-5">{count}</span>
                 <Button variant="outline-success" size="sm" onClick={add}><Add /></Button>
-                </div>                        
-                <div className="d-grid gap-2">
-                    {
-                        stock
-                        ?<Button variant="primary" size="lg" onClick={() => onAdd(count)}>Add to Cart</Button>
-                        :<Button variant="danger" size="lg" disabled>Out of Stock</Button>
-                    }                         
-                </div>
-            </div>
+                <hr></hr>
+                {
+                    stock
+                    ?<Button variant="primary" size="lg" onClick={() => onAdd(count)}>Add to Cart</Button>
+                    :<Button variant="danger" size="lg" disabled>Out of Stock</Button>
+                }
+           </Card.Text>
+           
         );    
 }
