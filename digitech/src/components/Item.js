e6fdbdaa330643fe9570d963e5b2b3e5f5601fdf
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-04-04 22:50:34 -0300

    Update Item.js

--- digitech/src/components/Item.js
+++ digitech/src/components/Item.js
@@ -3,16 +3,21 @@ import ItemCount from "./ItemCount";
 
 export default function Item ({ id, title, stock, cost, brand, image }) {
 
+      //Mostramos una alerta al agregar los items
+    const onAdd = (items) => {
+        alert(`${items} items added to cart`);
+    }
+
     return ( 
         <Col className="mt-3">
-            <Card key={id} >
+            <Card key={id} style={{ width: '19rem' }} >
                 <CardImg top width="100%" src={image} alt="Card image cap" />
                     <Card.Body>
                         <Card.Title className="text-center font-weight-bolder" style={{ color : 'blue'}}>{title}</Card.Title>
                         <Card.Subtitle className="text-center font-weight-bolder" style={{ color : 'red' }}>USD: {cost}</Card.Subtitle>
                         <Card.Text className="text-center"></Card.Text>
                         <Card.Text className="text-center">Brand: {brand}</Card.Text>                                
-                        <ItemCount />
+                        <ItemCount stock={5} initial={1} onAdd={onAdd}/>
                         <Card.Text className="text-center mt-2">Stock: {stock}</Card.Text>
                     </Card.Body>
                 </Card>
