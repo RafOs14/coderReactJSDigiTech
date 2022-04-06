463fb388737aed902c34b64f533df35cc67b8c7b
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-04-06 16:47:38 -0300

    Update ItemDetail.js

--- digitech/src/components/ItemDetail.js
+++ digitech/src/components/ItemDetail.js
@@ -1,6 +1,7 @@
 import ItemCount from "./ItemCount";
 import { Col, Container, Card, CardImg, Row } from "react-bootstrap";
 import React from "react";
+import { CardContent } from "@material-ui/core";
 
 const ItemDetail = ({ id, title, desc, stock, cost, brand, image }) => {
 
@@ -9,28 +10,31 @@ const ItemDetail = ({ id, title, desc, stock, cost, brand, image }) => {
         alert(`${items} items added to cart`);
     }
 
-    return(
-        <>               
-            <Container className="mt-3">
-                <Row>
-                    <Col>
-                        <CardImg top width="100%" src={image} alt="" />
-                    </Col>
-                    <Col>
-                        <Card style={{ width : 'auto' }}>
-                            <Card.Header className="text-center font-weight-bolder" style={{ color : 'blue'}}>{title}</Card.Header>
-                            <Card.Body> 
-                                <Card.Text className="font-weight-bolder" style={{ color : 'blue' }}> {desc}</Card.Text>                     
-                                <Card.Text className="font-weight-bolder" style={{ color : 'red' }}>USD: {cost}</Card.Text>                     
-                                <Card.Text className="text-center">Brand: {brand}</Card.Text>
-                                <Card.Text className="text-center mt-2">Stock en depósito: {stock}</Card.Text>
-                                <ItemCount stock={stock} initial={1} onAdd={onAdd}/>
-                            </Card.Body>
-                        </Card>
-                    </Col>                    
-                </Row>
-            </Container>
-        </>
+    return(                 
+        <Container className="mt-3">
+            <Row>
+                <Col sm={4}>
+                    <CardImg top width="100%" src={image} alt="" />
+                </Col>
+                <Col sm={8}>
+                    <Card style={{ width : 'auto' }}>
+                        <Card.Header className="text-center font-weight-bolder" style={{ color : 'blue'}}>{title}</Card.Header>
+                        <Card.Body> 
+                            <Card.Text className="font-weight-bolder" style={{ color : 'blue' }}> {desc}</Card.Text>                     
+                            <Card.Text className="font-weight-bolder" style={{ color : 'red' }}>USD: {cost}</Card.Text>                     
+                            <Card.Text className="text-center">Brand: {brand}</Card.Text>
+                            <Card.Text className="text-center mt-2">Stock en depósito: {stock}</Card.Text>
+                            <CardContent className="align-items-right">
+                                <Row>
+                                    <Col></Col>
+                                    <Col><ItemCount stock={stock} initial={1} onAdd={onAdd}/></Col>
+                                </Row>
+                            </CardContent>
+                        </Card.Body>
+                    </Card>
+                </Col>                    
+            </Row>
+        </Container>       
     );
 }
 
