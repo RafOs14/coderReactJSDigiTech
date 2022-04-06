555991bb9d4370742c93872a0e3a690cab5fe2c1
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-04-06 15:35:30 -0300

    Update ItemList.js

--- digitech/src/components/ItemList.js
+++ digitech/src/components/ItemList.js
@@ -5,11 +5,11 @@ import Item from "./Item";
 const ItemList = ({ items }) => {
     return (
         <Container>  
-            <Row className="m-auto align-self-center">                    
+            <Row>                    
                 { 
                     //Recorremos el arreglo de productos para mostrarlo en pantalla
                     items.length > 0
-                    ?items.map(item => <Item key={item.id} id={item.id} title={item.description} cost={item.price} brand={item.brand} image={item.image[0]} stock={item.stock} />)
+                    ?items.map(item => <Item key={item.id} id={item.id} title={item.name} desc={item.description} cost={item.price} brand={item.brand} image={item.image[0]} stock={item.stock} />)
                     : <div className="text-center mt-3">Products will be displayed in an instant...</div>                    
                 } 
             </Row>
