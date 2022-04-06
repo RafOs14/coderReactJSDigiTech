7c194561c890c424f1a7b10ae2dbb5633353616e
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-04-06 10:59:01 -0300

    Update ItemCount.js

--- digitech/src/components/ItemCount.js
+++ digitech/src/components/ItemCount.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import { Add, Remove } from '@material-ui/icons'
 
 //Declaramos la clase y lo que se va a renderizar
@@ -9,7 +9,8 @@ export default function ItemCount ({stock, initial, onAdd}) {
         
         useEffect(() => {
             setCount(initial);
-        }, [initial]);
+        }, []);
+
         //Funcion que se ejecuta cuando se hace click en el boton de restar
         const remove = () => {
             if(count > initial){
@@ -24,17 +25,19 @@ export default function ItemCount ({stock, initial, onAdd}) {
         }
         //Retornamos el componente
         return (
-           <Card.Text className="text-center">
-               <Button variant="outline-danger" size="sm" onClick={remove}><Remove /></Button>
-                <span className="mx-5">{count}</span>
+            <div className="d-grid gap-2">
+                <div className="mx-4 text-center">
+                <Button variant="outline-danger" size="sm" onClick={remove}><Remove /></Button>
+                    <span className="mx-5">{count}</span>
                 <Button variant="outline-success" size="sm" onClick={add}><Add /></Button>
-                <hr></hr>
-                {
-                    stock
-                    ?<Button variant="primary" size="lg" onClick={() => onAdd(count)}>Add to Cart</Button>
-                    :<Button variant="danger" size="lg" disabled>Out of Stock</Button>
-                }
-           </Card.Text>
-           
+                </div>                        
+                <div className="d-grid gap-2 mt-2">
+                    {
+                        stock
+                        ?<Button variant="primary" size="lg" onClick={() => onAdd(count)}>Add to Cart</Button>
+                        :<Button variant="danger" size="lg" disabled>Out of Stock</Button>
+                    }                         
+                </div>
+            </div>
         );    
 }
