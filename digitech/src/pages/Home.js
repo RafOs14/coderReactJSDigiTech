907418c71a50323927ac8f951684b1e109b0f043
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-04-07 18:51:19 -0300

    Update Home.js

--- digitech/src/pages/Home.js
+++ digitech/src/pages/Home.js
@@ -1,14 +1,18 @@
 import NavBar from '../components/NavBar';
 import ItemListContainer from '../components/ItemListContainer';
 import ItemDetailContainer from '../components/ItemDetailContainer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const  Home = () => {
   return (
-      <>
+      <BrowserRouter>
         <NavBar />
-        {/*<ItemListContainer />*/}
-        <ItemDetailContainer />
-      </>        
+        <Routes>
+          <Route path="/" element={<ItemListContainer />} />
+          <Route path="/category/:idCategory" element={<ItemListContainer />} />
+          <Route path="/item/:id" element={<ItemDetailContainer />} />          
+        </Routes>
+      </BrowserRouter>       
   );
         
 }
