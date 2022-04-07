f9bccf2967fba96a4478cc1677db6c886be46794
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-04-07 18:50:59 -0300

    Update NavBar.js

--- digitech/src/components/NavBar.js
+++ digitech/src/components/NavBar.js
@@ -2,13 +2,14 @@
 import logo from "../assets/logo.png";
 import CartWidget from "./CartWidget";
 import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Link } from "@material-ui/core";
 
 const NavBar = () => {
         /*Retornamos lo que va a mostrar el componente principal*/
         return ( 
             <Navbar bg="dark" variant="dark" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home" gap={2}>
+                    <Link to='/'><Navbar.Brand gap={2}>
                         <img
                         alt=""
                         src={logo}
@@ -17,18 +18,18 @@ const NavBar = () => {
                         className="d-inline-block align-top"
                         />{' '}
                         DigiTech
-                    </Navbar.Brand>
+                    </Navbar.Brand></Link>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Computer</Nav.Link>
-                        <Nav.Link href="#link">Video Games</Nav.Link> 
-                        <Nav.Link href="#link">Cell Phones</Nav.Link>        
+                        <Nav.Link to='/'>Computer</Nav.Link>
+                        <Nav.Link to='/'>Video Games</Nav.Link> 
+                        <Nav.Link to='/'>Cell Phones</Nav.Link>        
                     </Nav>                   
                     <Nav className="mx-4">
-                        <Nav.Link href="#link">Register</Nav.Link>
-                        <Nav.Link href="#link">Sign In</Nav.Link> 
-                        <Nav.Link href="#link">Sign Up</Nav.Link>                         
+                        <Nav.Link to='/'>Register</Nav.Link>
+                        <Nav.Link to='/'>Sign In</Nav.Link> 
+                        <Nav.Link to='/'>Sign Up</Nav.Link>                         
                     </Nav>                    
                     <CartWidget />
                     </Navbar.Collapse>
