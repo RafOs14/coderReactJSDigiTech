311383144547ce2c423a05eebf00ac4c47d4b4d3
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2022-04-11 10:40:45 -0300

    Update ItemDetail.js

--- digitech/src/components/ItemDetail.js
+++ digitech/src/components/ItemDetail.js
@@ -3,7 +3,7 @@ import { Col, Container, Card, CardImg, Row } from "react-bootstrap";
 import React from "react";
 import { CardContent } from "@material-ui/core";
 
-const ItemDetail = ({ id, title, desc, stock, cost, brand, image }) => {
+const ItemDetail = ({ item }) => {
 
     //Mostramos una alerta al agregar los items
     const onAdd = (items) => {
@@ -11,31 +11,39 @@ const ItemDetail = ({ id, title, desc, stock, cost, brand, image }) => {
     }
 
     return(                 
+        <>
+        {item && item.image
+            ?               
         <Container className="mt-3">
             <Row>
                 <Col sm={4}>
-                    <CardImg top width="100%" src={image} alt="" />
+                    <CardImg top width="100%" src={item.image} alt="" />
                 </Col>
                 <Col sm={8}>
                     <Card style={{ width : 'auto' }}>
-                        <Card.Header className="text-center font-weight-bolder" style={{ color : 'blue'}}>{title}</Card.Header>
+                        <Card.Header className="text-center font-weight-bolder" style={{ color : 'blue'}}>{item.name}</Card.Header>
                         <Card.Body> 
-                            <Card.Text className="font-weight-bolder"> {desc}</Card.Text>                     
-                            <Card.Text className="fs-3 fw-bold" style={{ color : 'red' }}>USD: {cost}</Card.Text>                                                       
+                            <Card.Text className="font-weight-bolder"> {item.description}</Card.Text>                     
+                            <Card.Text className="fs-3 fw-bold" style={{ color : 'red' }}>USD: {item.price}</Card.Text>                                                       
                             <CardContent className="align-items-right">
                                 <Row>
                                     <Col>
-                                        <Card.Text className="text-center">Brand: {brand}</Card.Text>
-                                        <Card.Text className="text-center mt-2">Stock en depósito: {stock}</Card.Text>
+                                        <Card.Text className="text-center">Brand: {item.brand}</Card.Text>
+                                        <Card.Text className="text-center mt-2">Stock en depósito: {item.stock}</Card.Text>
                                     </Col>
-                                    <Col><ItemCount stock={stock} initial={1} onAdd={onAdd}/></Col>
+                                    <Col>
+                                         <ItemCount stock={item.stock} initial={1} onAdd={onAdd}/>
+                                    </Col>
                                 </Row>
                             </CardContent>
                         </Card.Body>
                     </Card>
                 </Col>                    
             </Row>
-        </Container>       
+        </Container> 
+        : <div className="text-center mt-3">Products will be displayed in an instant...</div>
+        }    
+        </>        
     );
 }
 
